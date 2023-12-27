055c76cb3a19684b0d1d1384c658304886939bf4
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-12-27 21:10:43 +0500

    Change title tests in CreditPageTest

--- src/test/java/ru/netology/tests/CreditPageTest.java
+++ src/test/java/ru/netology/tests/CreditPageTest.java
@@ -37,7 +37,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("1.1. Оплата тура в кредит валидной картой со статусом 'APPROVED'")
-    void testPayAllFieldsValidApprovedCardNumber() {
+    void testCreditAllFieldsValidApprovedCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getApprovedCard());
@@ -47,7 +47,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("1.2. Оплата тура в кредит невалидной картой с статусом DECLINED")
-    void testPayAllFieldsValidDeclinedCardNumber() {
+    void testCreditAllFieldsValidDeclinedCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getDeclinedCard());
@@ -57,7 +57,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.1. Оплата тура в кредит по несуществующей карте.")
-    void testPayAllFieldsValidRandomCardNumber() {
+    void testCreditAllFieldsValidRandomCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidCardNumberRandom());
@@ -67,7 +67,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.2. Оплата тура в кредит картой с истекшим сроком действия(месяц).")
-    void testPayAllFieldsValidExpiredMonth() {
+    void testCreditAllFieldsValidExpiredMonth() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getLastMonth());
@@ -76,7 +76,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.3. Оплата тура в кредит картой с истекшим сроком действия(год).")
-    void testPayAllFieldsValidExpiredYear() {
+    void testCreditAllFieldsValidExpiredYear() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getLastYear());
@@ -85,7 +85,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.4. Оплата тура в кредит картой с 15ю символами.")
-    void testPay15DigitsCardNumber() {
+    void testCredit15DigitsCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.get15DigitsCardNumber());
@@ -94,7 +94,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.5. Ввод номера карты с 17ю символами.")
-    void testPay17DigitsCardNumber() {
+    void testCredit17DigitsCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.get17DigitsCardNumber());
@@ -103,7 +103,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.6. Оплата тура в кредит, если поле НОМЕР КАРТЫ пустое.")
-    void testPayAllFieldsValidCardNumberNull() {
+    void testCreditAllFieldsValidCardNumberNull() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getEmptyCardNumber());
@@ -112,7 +112,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.7. Ввод номера карты с буквенным символом.")
-    void testPayAllFieldsValidLiteralCharCardNumber() {
+    void testCreditAllFieldsValidLiteralCharCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getLiteralCharCardNumber());
@@ -121,7 +121,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.8. Ввод номера карты со спецсимволом.")
-    void testPayAllFieldsValidSpecCharCardNumber() {
+    void testCreditAllFieldsValidSpecCharCardNumber() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getSpecCharCardNumber());
@@ -130,7 +130,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.9. Ввод в поле МЕСЯЦ 00.")
-    void testPayAllFieldsValidZeroMonth() {
+    void testCreditAllFieldsValidZeroMonth() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getZeroMonth());
@@ -139,7 +139,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.10. Оставить поле МЕСЯЦ пустым.")
-    void testPayAllFieldsValidEmptyMonth() {
+    void testCreditAllFieldsValidEmptyMonth() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getEmptyMonth());
@@ -148,7 +148,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.11. Ввод в поле МЕСЯЦ буквенного символа.")
-    void testPayAllFieldsValidLiteralCharMonth() {
+    void testCreditAllFieldsValidLiteralCharMonth() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getLiteralCharMonth());
@@ -157,7 +157,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.12. Ввод в поле месяц МЕСЯЦ спецсимвола.")
-    void testPayAllFieldsValidSpecCharMonth() {
+    void testCreditAllFieldsValidSpecCharMonth() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getSpecCharMonth());
@@ -166,7 +166,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.13. Оставить поле ГОД пустым.")
-    void testPayAllFieldsValidEmptyYear() {
+    void testCreditAllFieldsValidEmptyYear() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getEmptyYear());
@@ -175,7 +175,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.14. Ввод в поле ГОД буквенного символа.")
-    void testPayAllFieldsValidLiteralCharYear() {
+    void testCreditAllFieldsValidLiteralCharYear() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getLiteralCharYear());
@@ -184,7 +184,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.15. Ввод в поле месяц ГОД спецсимвола.")
-    void testPayAllFieldsValidSpecCharYear() {
+    void testCreditAllFieldsValidSpecCharYear() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getSpecCharYear());
@@ -193,7 +193,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.16. Ввод в поле ГОД значения +6 лет от текущего года.")
-    void testPayAllFieldsValidPlusSixYear() {
+    void testCreditAllFieldsValidPlusSixYear() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidYearPlus6());
@@ -202,7 +202,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.17. Оставить поле ВЛАДЕЛЕЦ пустым.")
-    void testPayAllFieldsValidEmptyHolder() {
+    void testCreditAllFieldsValidEmptyHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getEmptyHolder());
@@ -211,7 +211,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.18. Ввод в поле ВЛАДЕЛЕЦ цифр.")
-    void testPayAllFieldsValidDigitsHolder() {
+    void testCreditAllFieldsValidDigitsHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidHolderDigits());
@@ -220,7 +220,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.19. Ввод в поле Владелец спецсимвола.")
-    void testPayAllFieldsValidSpecCharHolder() {
+    void testCreditAllFieldsValidSpecCharHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidHolderSpecChar());
@@ -229,7 +229,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.20. Ввод в поле ВЛАДЕЛЕЦ фамилии и имени без пробела.")
-    void testPayAllFieldsValidWithoutSpaceHolder() {
+    void testCreditAllFieldsValidWithoutSpaceHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidHolderOneName());
@@ -238,7 +238,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.21. Поставить пробел перед вводом значения в поле ВЛАДЕЛЕЦ.")
-    void testPayAllFieldsValidSpaceOverHolder() {
+    void testCreditAllFieldsValidSpaceOverHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidHolderSpaceOverHolder());
@@ -247,7 +247,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.22. Поставить пробел после ввода значения в поле ВЛАДЕЛЕЦ.")
-    void testPayAllFieldsValidSpaceBehindHolder() {
+    void testCreditAllFieldsValidSpaceBehindHolder() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidHolderSpaceAfterHolder());
@@ -256,7 +256,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.23. Оставить поле CVC/CVV пустым.")
-    void testPayAllFieldsValidEmptyCVV() {
+    void testCreditAllFieldsValidEmptyCVV() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getEmptyCVV());
@@ -265,7 +265,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.24. Ввод в поле CVC/CVV 1 цифры.")
-    void testPayAllFieldsValidOneDigitsCVV() {
+    void testCreditAllFieldsValidOneDigitsCVV() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getOneNumberCVV());
@@ -274,7 +274,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.25. Ввод в поле CVC/CVV 2х цифр.")
-    void testPayAllFieldsValidTwoDigitsCVV() {
+    void testCreditAllFieldsValidTwoDigitsCVV() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getTwoNumberCVV());
@@ -283,7 +283,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.26. Ввод в поле CVC/CVV буквенного символа.")
-    void testPayAllFieldsValidLiteralCharCVV() {
+    void testCreditAllFieldsValidLiteralCharCVV() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidCVVLiteralChar());
@@ -292,7 +292,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.27. Ввод в поле CVC/CVV спецсимвола.")
-    void testPayAllFieldsValidSpecCharCVV() {
+    void testCreditAllFieldsValidSpecCharCVV() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.fillFields(DataHelper.getInvalidCVVSpecChar());
@@ -301,7 +301,7 @@ public class CreditPageTest {
 
     @Test
     @DisplayName("2.28. Оплата тура в кредит с пустыми полями.")
-    void testPayAllFieldsEmpty() {
+    void testCreditAllFieldsEmpty() {
         var dashboardPage = new DashboardPage();
         var credit = dashboardPage.openCreditPage();
         credit.clearFields();
